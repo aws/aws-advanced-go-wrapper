dc21f974a8db7cfeb070a41f5e85c3da5b13d083
aaronchung-bitquill <118320132+aaronchung-bitquill@users.noreply.github.com>
2025-02-10 15:35:24 -0800

    Chore/poc (#4)

---
+++ driver.go
@@ -0,0 +1,64 @@
+package driver
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"github.com/go-sql-driver/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type AwsWrapperDriver struct {
+	targetDriver  driver.Driver
+	pluginManager PluginManager
+}
+
+func (d AwsWrapperDriver) Open(dsn string) (driver.Conn, error) {
+	// do wrapper driver things
+	d.pluginManager.Init()
+
+	// call underlying driver
+	conn, err := d.targetDriver.Open(dsn)
+	return AwsWrapperConn{underlyingConn: conn, pluginManager: d.pluginManager}, err
+}
+
+var driverName = "aws"
+
+func init() {
+	if driverName != "" {
+		var targetDriver = &mysql.MySQLDriver{}
+		sql.Register(
+			driverName,
+			&AwsWrapperDriver{
+				targetDriver,
+				&ConnectionPluginManager{targetDriver: targetDriver}})
+	}
+}
+
+type AwsWrapperConn struct {
+	underlyingConn driver.Conn
+	pluginManager  PluginManager
+}
+
+func (c AwsWrapperConn) Prepare(query string) (driver.Stmt, error) {
+	// do wrapper driver things
+
+	// call underlying driver
+	//return c.underlyingConn.Prepare(query)
+	prepareFunc := func() (driver.Stmt, error) { return c.underlyingConn.Prepare(query) }
+	return c.pluginManager.Prepare(prepareFunc)
+}
+
+func (c AwsWrapperConn) Close() error {
+	// close wrapper driver things
+
+	// close underlying driver
+	return c.underlyingConn.Close()
+}
+
+func (c AwsWrapperConn) Begin() (driver.Tx, error) {
+	// do wrapper driver things
+
+	// call underlying driver
+	return c.underlyingConn.Begin()
+}
---
+++ driver_test.go
@@ -0,0 +1,12 @@
+package driver
+
+import "testing"
+
+func TestDummy(t *testing.T) {
+	got := 1
+	want := 1
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
