4e98ed3b439c7f478926263541dc75233d75ad9e
Favian Samatha <faviansamatha@yahoo.com>
2026-03-31 15:24:36 -0700

    fix: iam region creds provider

--- auth-helpers/aws_client_helper.go
+++ auth-helpers/aws_client_helper.go
@@ -18,7 +18,10 @@ package auth_helpers
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
@@ -30,6 +33,21 @@ type AwsStsClient interface {
 type AwsStsClientProvider func(region string) AwsStsClient
 
 func NewAwsStsClient(region string) AwsStsClient {
-	options := sts.Options{Region: region}
-	return sts.New(options)
+	if region != "" {
+		return sts.New(sts.Options{
+			Region:      region,
+			Credentials: aws.AnonymousCredentials{},
+		})
+	}
+
+	// When region is empty, fall back to the default AWS config chain for region resolution
+	// (env vars, ~/.aws/config, EC2 metadata, etc.) but still use anonymous credentials.
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		slog.Warn("Failed to load default AWS config, creating STS client without region", "error", err)
+		return sts.New(sts.Options{Credentials: aws.AnonymousCredentials{}})
+	}
+	return sts.NewFromConfig(cfg, func(o *sts.Options) {
+		o.Credentials = aws.AnonymousCredentials{}
+	})
 }
